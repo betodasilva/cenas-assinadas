feb3957e57ea48e1e385e319750344c7a4530326
betodasilva <jrs_robert@hotmail.com>
2017-11-29 20:03:31 -0300

    (finally) hotfix - child theme style

--- themes/cenas-assinadas/functions.php
+++ themes/cenas-assinadas/functions.php
@@ -4,11 +4,14 @@
         $parent_style = 'rango-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
         wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-        wp_enqueue_style( 'cenas-assinadas-style',
-            get_stylesheet_uri(),
-            NULL,
-            filemtime( get_stylesheet_directory() . '/style.css' )
+        wp_enqueue_style( 'rango-child-style',
+            get_stylesheet_directory_uri() . '/style.css',
+            array( $parent_style ),
+            wp_get_theme()->get('Version')
         );
     }
     add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', PHP_INT_MAX );
 ?>
+
+
+
