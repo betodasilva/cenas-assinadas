8f44ec79003f17cd1e176373154ac4c49420cd14
betodasilva <jrs_robert@hotmail.com>
2017-11-29 17:54:32 -0300

    fix for child stylesheet

--- themes/cenas-assinadas/functions.php
+++ themes/cenas-assinadas/functions.php
@@ -5,9 +5,9 @@
 
         wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
         wp_enqueue_style( 'cenas-assinadas-style',
-            get_stylesheet_directory_uri() . '/style.css',
-            array( $parent_style ),
-            wp_get_theme()->get('Version')
+            get_stylesheet_uri(),
+            NULL,
+            filemtime( get_stylesheet_directory() . '/style.css' )
         );
     }
     add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
